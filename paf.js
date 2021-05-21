c6a8b23e2709b7475fbdbb3d2e10eb1a93a9cd89
AngelaSophia <ayugisophiaangela@gmail.com>
2021-05-21 13:41:37 +0300

    performance

---
+++ paf.js
@@ -0,0 +1,5 @@
+    I have planed to wake up at 5:00am to go through the past code we have 
+    done and  have made a time table that will guid me to work on that.
+    Am going to start on one project of javascript on making a watch that is functioning.
+    I have a group that will help me to go through the code and any other academic work.
+    You can go through it and change what is not GeolocationCoordinates.
