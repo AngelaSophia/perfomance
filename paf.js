c496a21b4dfe4adc02a6f525e26299cd99ce7ea9
AngelaSophia <ayugisophiaangela@gmail.com>
2021-05-21 13:44:43 +0300

    changes

--- paf.js
+++ paf.js
@@ -2,4 +2,4 @@
     done and  have made a time table that will guid me to work on that.
     Am going to start on one project of javascript on making a watch that is functioning.
     I have a group that will help me to go through the code and any other academic work.
-    You can go through it and change what is not GeolocationCoordinates.
+    You can go through it and change what is not good.
